397448c6cf2083f38d032ba95b6d62f5b9eee310
JuanAndres17 <jacastellanosc@udistrital.edu.co>
2024-08-23 13:50:27 -0500

    Avanzando1

--- Condicion.java
+++ Condicion.java
@@ -1,12 +1,19 @@
 import java.util.Scanner;
 
 class Condicion {
-  public static void main(String[] args) {
-    Scanner leer = new Scanner(System.in);
-    System.out.println("Ingrese un numero");
-    int n1 = leer.nextInt();
-    System.out.println("Ingrese otro numero");
-    int n2 = leer.nextInt();
+ public static void main(String[] args) {
+ Scanner leer = new Scanner(System.in);
+ Int day = 23
+ Int month = 08
+ Int year = 2024
+    System.out.print.ln("Ingrese su nombre");
+String name = leer.next();
+System.out.print.ln("Ingrese su anho de nacimiento");
+int anho = leer.nextInt();
+System.out.println("Ingrese su mes de nacimiento");
+int mes = leer.nextInt();
+
+
 
     if(n1>n2)
     {
